91f0abe7bc09311426126d460ffd54b2852747bb
Alexandre Morais <alexandre@a2o.com.br>
2021-08-25 13:57:37 -0300

    Codificado o segundo desafio.

--- src/index.js
+++ src/index.js
@@ -10,15 +10,37 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: 'User not exists!' });
+  }
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { pro, todos } = request.user;
+  if (!pro && todos.length() === 10 ) {
+    return response.status(403).json({erro: 'Limite do plano gratuíto atingido!'});
+  }
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+  const todo = user.todos.find(todo => todo.id === id);
+  const uuidIsValid = validate(id);
+  if (!uuidIsValid) {
+    return response.status(400).json({ error: 'ID inválido!' });
+  }
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not exists!' });
+  }
+  request.user = user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
